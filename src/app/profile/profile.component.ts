fd1cdee09544a290d12c9fd6b3efe38ae45ad891
Cesar Zaragoza <zaragoza0711@gmail.com>
2025-01-27 20:38:46 -0600

    actualizado

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -5,30 +5,29 @@ import { FormsModule } from '@angular/forms';
   selector: 'app-profile',
   imports: [FormsModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
 export class ProfileComponent {
   //atributos
   user = {
-  firstName: "Juan",
-  lastName: "Perez",
-  email: "juan.perez@example.com",
-  username: "juanpz",
-  imageUrl: "/chico.jpg",
+    firstName: 'Juan',
+    lastName: 'Perez',
+    email: 'juan.perez@example.com',
+    username: 'juanpz',
+    imageUrl: '/chico.jpg',
   };
 
   //metodos
   // Cambio de Imagen
   onImageChange(event: Event) {
-  const file = (event.target as HTMLInputElement).files?.[0];
-  if (file) {
-    
-  const reader = new FileReader();
-  reader.onload = ( ) => {
-  this.user.imageUrl = reader.result as string;
-  };
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.user.imageUrl = reader.result as string;
+      };
 
-  reader.readAsDataURL(file);
-  }//fin del if
-  }//fin del metodo
+      reader.readAsDataURL(file);
+    } //fin del if
+  } //fin del metodo
 }
